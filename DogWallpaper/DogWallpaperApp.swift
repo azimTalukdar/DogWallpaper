e4b683ce4b7dc494dee7981672d4c4246b455ef1
Azim Talukdar <azimtalukdar@gofynd.com>
2024-03-01 12:30:07 +0530

    Initial Commit

---
+++ DogWallpaper/DogWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogWallpaperApp.swift
+//  DogWallpaper
+//
+//  Created by Azim Talukdar on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DogWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
